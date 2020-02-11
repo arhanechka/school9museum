6ffcb76b8713f2fdf2045210e3c43ac8d36c3ce7
Arhanechka <aranvic1976@gmail.com>
2020-02-11 07:28:33 +0200

    Initial commit

---
+++ precache-manifest.6c59dd139cd68c846a0640eb910cf631.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714eac537aee38fe2bee0a219216fd81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e29cad2694487abe6286",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "98faaee186c95fbeb653",
+    "url": "/static/css/main.2ed6974d.chunk.css"
+  },
+  {
+    "revision": "e29cad2694487abe6286",
+    "url": "/static/js/2.8a1323b7.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.8a1323b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98faaee186c95fbeb653",
+    "url": "/static/js/main.3a09070f.chunk.js"
+  },
+  {
+    "revision": "d5ca5dc0d9f42c5a9290",
+    "url": "/static/js/runtime-main.44762092.js"
+  }
+]);
